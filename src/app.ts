de7e8c1178059b31615ed9ab5126543fae97edfe
Darlisson Limeira <darlisson.dev@gmail.com>
2023-02-09 11:19:30 -0300

    setup product router in app

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { notFoundErrorHandler } from "./middleware/notFoundErrorHandler";
 import { serverErrorHandler } from "./middleware/serverErrorHandler";
+import { productRouter } from "./routes/productRouter";
 import { userRouter } from "./routes/userRouter";
 
 const app = express();
@@ -8,6 +9,7 @@ const app = express();
 app.use(express.json());
 
 app.use("/users", userRouter);
+app.use("/products", productRouter);
 
 app.use(notFoundErrorHandler);
 
