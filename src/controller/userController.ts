e19bb6ef78bb2e705c6eeee2f33fb1c980b1b11f
Darlisson Limeira <darlisson.dev@gmail.com>
2023-02-09 10:27:46 -0300

    improve user controller
    add type to response data

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -1,11 +1,12 @@
 import { NextFunction, Request, RequestHandler, Response } from "express";
 import { mockend } from "../config/api";
+import { User } from "../types/User";
 import { CustomResponse } from "../utils/CustomResponse";
 
 export const getUsers: RequestHandler = async (req, res, next) => {
     try {
         const response = await mockend.get("users");
-        const users = response.data;
+        const users: User[] = response.data;
         return res.status(200).json(new CustomResponse("success", users));
     } catch (error) {
         next(error);
