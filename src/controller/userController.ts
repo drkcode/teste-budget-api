d3b6c37be91a79bd38c2d07de32edd37d1a53532
Darlisson Limeira <darlisson.dev@gmail.com>
2023-02-15 23:58:04 -0300

    refactor user controller
    user controller now uses a user service instance

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -1,12 +1,13 @@
-import { NextFunction, Request, RequestHandler, Response } from "express";
+import { RequestHandler } from "express";
 import { mockend } from "../config/api";
+import { UserService } from "../services/userService";
 import { User } from "../types/User";
 import { CustomResponse } from "../utils/CustomResponse";
 
 export const getUsers: RequestHandler = async (req, res, next) => {
     try {
-        const response = await mockend.get("users");
-        const users: User[] = response.data;
+        const userService = new UserService(mockend);
+        const users: User[] = await userService.getUsers();
         return res.status(200).json(new CustomResponse("success", users));
     } catch (error) {
         next(error);
